c937a32679e67f2f4dce954a6356aa5b055f7a4d
David Blanton <jdavidblanton@gmail.com>
2026-04-05 17:24:23 -0400

    Use direct image imports instead of index file

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import btnSmallCopper from '../assets/btn_small_copper.png';
+import btnLongCopper from '../assets/btn_long_copper.png';
+import btnSmallGm from '../assets/btn_small_gm.png';
+import btnLongGm from '../assets/btn_long_gm.png';
 
 interface CustomButtonProps {
   type: "copper" | "gm";
@@ -7,64 +11,17 @@ interface CustomButtonProps {
   onClick?: () => void;
 }
 
-export const CustomButton: React.FC<CustomButtonProps> = ({
-  type,
-  size,
-  text,
-  onClick,
-}) => {
+export const CustomButton: React.FC<CustomButtonProps> = ({ type, size, text, onClick }) => {
   let imageSrc = "";
-
-  if (type === "copper" && size === "small") {
-    imageSrc = "/src/assets/btn_small_copper.png";
-  } else if (type === "copper" && size === "long") {
-    imageSrc = "/src/assets/btn_long_copper.png";
-  } else if (type === "gm" && size === "small") {
-    imageSrc = "/src/assets/btn_small_gm.png";
-  } else if (type === "gm" && size === "long") {
-    imageSrc = "/src/assets/btn_long_gm.png";
-  }
+  if (type === "copper" && size === "small") imageSrc = btnSmallCopper;
+  else if (type === "copper" && size === "long") imageSrc = btnLongCopper;
+  else if (type === "gm" && size === "small") imageSrc = btnSmallGm;
+  else if (type === "gm" && size === "long") imageSrc = btnLongGm;
 
   return (
-    <button
-      onClick={onClick}
-      style={{
-        position: "relative",
-        background: "none",
-        border: "none",
-        cursor: "pointer",
-        padding: 0,
-        width: "100%",
-      }}
-    >
-      <img
-        src={imageSrc}
-        alt={`${type} ${size} button`}
-        style={{
-          display: "block",
-          width: "100%",
-          height: "auto",
-        }}
-        onError={(e) => {
-          console.error(`Button image failed to load: ${imageSrc}`);
-          e.currentTarget.style.display = "none";
-        }}
-      />
-      <span
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          color: "#FFFFFF",
-          fontSize: size === "small" ? "14px" : "16px",
-          fontWeight: 600,
-          whiteSpace: "nowrap",
-          textShadow: "1px 1px 2px rgba(0,0,0,0.5)",
-        }}
-      >
-        {text}
-      </span>
+    <button onClick={onClick} style={{ position: "relative", background: "none", border: "none", cursor: "pointer", padding: 0 }}>
+      <img src={imageSrc} alt="button" style={{ display: "block", width: "100%", height: "auto" }} />
+      <span style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "#FFF", fontSize: size === "small" ? "14px" : "16px", fontWeight: 600, whiteSpace: "nowrap", textShadow: "1px 1px 2px rgba(0,0,0,0.5)" }}>{text}</span>
     </button>
   );
 };
